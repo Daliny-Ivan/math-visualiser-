f78ca773ed8445e3245c7cd99caec5273dec0bc5
NeonBlurp <dhara.rane130@gmail.com>
2026-01-02 23:25:30 +0530

    Update 1_Trignometry.py

--- pages/1_Trignometry.py
+++ pages/1_Trignometry.py
@@ -4,6 +4,39 @@ import streamlit as st
 import numpy as np
 import plotly.graph_objects as go
 
+#-----BG-----------
+
+sd=os.path.dirname(os.path.abspath(__file__))
+bg_p=os.path.join(sd,"assests","bg_sciapp.png")
+
+if not os.path.exists(bg_p):
+    bg_p=os.path.join(sd,"assets","bg_sciapp.png")
+with open(bg_p,"rb") as f:
+    encoded= base64.b64encode(f.read()).decode()
+
+st.markdown(f"""
+<style>
+.stApp {{
+background-image: url("data:image/png;base64,{encoded}");
+background-size:cover;
+background-position:center;
+background-repeat: no-repeat;
+background-attachment:fixed;
+}}
+
+.block-container {{
+background-colour:rgba(0,0,0,0.55);
+padding: 2rem;
+border-radius:15px;
+}}
+h1,h2,h3,p,label{{
+colour:white !important;
+}}
+</style>
+""",unsafe_allow_html=True)
+
+
+# ------ PAGE LOGIC ---
 st.set_page_config(page_title="Trigonometry Lab", layout="centered")
 st.markdown(
     """
