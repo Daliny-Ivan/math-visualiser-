ecd10202411d41d0df408ed1fbef5cedb77bb248
NeonBlurp <dhara.rane130@gmail.com>
2026-01-02 23:27:15 +0530

    Update 4_Conic_Secs.py

--- pages/4_Conic_Secs.py
+++ pages/4_Conic_Secs.py
@@ -6,6 +6,39 @@ import plotly.graph_objects as go
 
 st.set_page_config(page_title="Conic Sections", layout="centered")
 
+# -------BG IMG--
+#-----BG-----------
+
+sd=os.path.dirname(os.path.abspath(__file__))
+bg_p=os.path.join(sd,"assests","bg_sciapp.png")
+
+if not os.path.exists(bg_p):
+    bg_p=os.path.join(sd,"assets","bg_sciapp.png")
+with open(bg_p,"rb") as f:
+    encoded= base64.b64encode(f.read()).decode()
+
+st.markdown(f"""
+<style>
+.stApp {{
+background-image: url("data:image/png;base64,{encoded}");
+background-size:cover;
+background-position:center;
+background-repeat: no-repeat;
+background-attachment:fixed;
+}}
+
+.block-container {{
+background-colour:rgba(0,0,0,0.55);
+padding: 2rem;
+border-radius:15px;
+}}
+h1,h2,h3,p,label{{
+colour:white !important;
+}}
+</style>
+""",unsafe_allow_html=True)
+#----end---
+
 # Dark background
 st.markdown(
     """
