714be74f7ce8efa6c5abe9309864e39dad9ca33d
NeonBlurp <dhara.rane130@gmail.com>
2026-01-02 23:24:08 +0530

    Update SCIAPP.py

--- SCIAPP.py
+++ SCIAPP.py
@@ -1,7 +1,39 @@
 #       S C I    A P P
 
 import streamlit as st
+import os
+import base64
+#-----BG-----------
 
+sd=os.path.dirname(os.path.abspath(__file__))
+bg_p=os.path.join(sd,"assests","bg_sciapp.png")
+
+if not os.path.exists(bg_p):
+    bg_p=os.path.join(sd,"assets","bg_sciapp.png")
+with open(bg_p,"rb") as f:
+    encoded= base64.b64encode(f.read()).decode()
+
+st.markdown(f"""
+<style>
+.stApp {{
+background-image: url("data:image/png;base64,{encoded}");
+background-size:cover;
+background-position:center;
+background-repeat: no-repeat;
+background-attachment:fixed;
+}}
+
+.block-container {{
+background-colour:rgba(0,0,0,0.55);
+padding: 2rem;
+border-radius:15px;
+}}
+h1,h2,h3,p,label{{
+colour:white !important;
+}}
+</style>
+""",unsafe_allow_html=True)
+#-----------Config log--
 st.set_page_config(page_title="Science App", layout="centered")
 st.markdown(
     """
@@ -16,8 +48,8 @@ st.markdown(
 )
 
 
-st.title("SCIENCE APP")
-st.subheader("Choose from below!!")
+st.title("M*ATH* V*ISUALISER*")
+st.subheader("Select from Below!")
 
 st.write("""
 Explore interactive graphs:
@@ -27,4 +59,5 @@ Explore interactive graphs:
 - Conic
 """)
 
+st.write("Visual and interactive graphs helps student understand the following mathematical concepts better.")
 st.info("Use the sidebar to navigate")
